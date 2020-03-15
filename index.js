2f0db84ff93e81e2a58ca9beade55ecf72fd146e
Yuxuan_Wang <36295581+YuxuanWang-CHINA@users.noreply.github.com>
2020-03-15 22:33:53 +0800

    v1.0.1

--- index.js
+++ index.js
@@ -6,30 +6,42 @@ function oneReq(token,aurl){
 }
 
 function downOne(token, turl) {
-    return axios.get(turl, { auth: { username: 'api', password: token } })
+    return axios.get(turl, { auth: { username: 'api', password: token } ,responseType: 'arraybuffer' })
 }
 
 function getBuf(token,urllist){
-    var returnbuf=[];
-    for (var i = 0; i < urllist.length; i++) {
-        downOne(token,urllist[i])
-    }
+    return new Promise((resolve, reject)=>{
+        var returnbuf=[];
+        for (let i = 0; i < urllist.length; i++) {
+            returnbuf.push(downOne(token,urllist[i]))
+        }
+        Promise.all(returnbuf)
+        .then((res)=>{
+            let re=[]
+            for (let i=0;i<res.length;i++ ){
+                re.push(res[i].data)
+            }
+            resolve(re)
+        })
+        .catch(e=>console.log(e))
+    })
 }
 
 var tinypnga = function(token,files)
 {
     return new Promise((resolve,reject) => {
         var reqlist=[]
-        for (var i=0;i < files.length;i++){
+        for (let i=0;i < files.length;i++){
             reqlist.push(oneReq(token,files[i]))
         }
         Promise.all(reqlist)
         .then((res)=>{
             let reslist = []
-            for (var i = 0; i < files.length; i++){
+            for (let i = 0; i < files.length; i++){
                 reslist.push(res[i].data.output.url)
             }
-            console.log(reslist)
+            getBuf(token,reslist)
+            .then((res)=>{resolve (res)})
         })
         .catch(e=>console.log(e));
     })
